6d3d70bbfdd753a27fc072af45a7b8bdc4018206
Nale-di <naledidironabeng@gmail.com>
2023-10-12 16:43:00 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -16,19 +16,3 @@ int main(void)
 
     return (0);
 }
-
-/**
- * positive_or_negative - Determines if an integer is positive, negative, or zero
- * @n: The integer to be checked
- *
- * Return: No return value
- */
-void positive_or_negative(int n)
-{
-    if (n > 0)
-        printf("%d is positive\n", n);
-    else if (n < 0)
-        printf("%d is negative\n", n);
-    else
-        printf("%d is zero\n", n);
-}
