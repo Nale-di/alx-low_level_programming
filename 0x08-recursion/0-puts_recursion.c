467a6d11e3677bc4667c22fd76519cb80915b3f2
Nale-di <naledidironabeng@gmail.com>
2023-10-25 16:05:25 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,19 @@
 #include "main.h"
+
 /**
- * _puts_recurssion - recursive function to print to screen
- *
- * @s: pointer to the string
- *
- * Return: nothing
- */
-void _puts_recurssion(char *s)
+  * _puts_recursion - main function
+  *
+  * @s: function parameter
+  *
+  * Return: Always 0.
+  */
+void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
-		_putschar(*s);
-		_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	}
-	if (*s == '\0')
-	{
-		_-putchar('\n');
-	}
+	else
+	_putchar('\n');
 }
