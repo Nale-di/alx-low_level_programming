4a676c4474d25f0043aaa8395cce7c3a931df9af
Nale-di <naledidironabeng@gmail.com>
2023-10-27 18:13:52 -0700

    Create the static library libmy.a containing all the functions

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,72 +1,21 @@
 #include "main.h"
-		
-/**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
- */
-int _isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
- 
- 
- ============================
- 
- 
-1-memcpy.c
-
-
-#include "main.h"
-/**
- *_memcpy - a function that copies memory area
- *@dest: memory where is stored
- *@src: memory where is copied
- *@n: number of bytes
- *
- *Return: copied memory with n byted changed
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	int r = 0;
-	int i = n;
-
-
-	for (; r < i; r++)
-	{
-		dest[r] = src[r];
-		n--;
-	}
-	return (dest);
-}
- 
- 
- 
-============================
- 
-1-strncat.c
-
-
-#include "main.h"
 /**
  * _strncat - concatenate two strings
  * using at most n bytes from src
  * @dest: input value
  * @src: input value
  * @n: input value
- *
  * Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
-{
+	char *_strncat(char *dest, char *src, int n)
+	{
 	int i;
 	int j;
-
 
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		i++;
+	i++;
 	}
 	j = 0;
 	while (j < n && src[j] != '\0')
@@ -77,4 +26,4 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	dest[i] = '\0';
 	return (dest);
-}
+	}
