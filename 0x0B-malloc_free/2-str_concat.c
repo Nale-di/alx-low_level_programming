bba4c8f3e7ae9f047250f9ce0dc28437d8252fa4
Nale-di <naledidironabeng@gmail.com>
2023-10-30 19:07:24 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,5 @@
 #include "main.h"
-		
-#include <stdlib.h>
-		
+#include <stdlib.h>		
 /** 
  * str_concat - get ends of input and add together for size
  * @s1: input one to concat
@@ -40,4 +38,4 @@
 		
 	conct[i] = '\0';
 	return (conct);
-		}
+	}
