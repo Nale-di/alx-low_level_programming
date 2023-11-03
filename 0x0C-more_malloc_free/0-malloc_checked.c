3903298641ea3b29b42e52c5a5be22239bd5b10d
Nale-di <naledidironabeng@gmail.com>
2023-11-02 18:17:37 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,43 +1,19 @@
 #include "main.h"
-/**
-* string_nconcat - a function that concatenates two strings.
-* @s1: first char
-* @s2: secound char
-* @n: unsigned int
-* Return: If the function fails, it should return NULL
-*/
-	char *string_nconcat(char *s1, char *s2, unsigned int n)
-	{	
-	unsigned int x, y, z;
-	char *s;
+#include <stdlib.h>
 
-	if (s1 == NULL)
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to the allocated memory.
+ */
+	void *malloc_checked(unsigned int b)
 	{
-	x = 0;
-	}
-	else
+	void *mem = malloc(b);
+
+	if (mem == NULL)
 	{
-	for (x = 0; s1[x]; ++x)
-	;
+	exit(98);
 	}
-	if (s2 == NULL)
-	{
-	y = 0;
-	}
-	else
-	{
-	for (y = 0; s2[y]; ++y)
-	;
-	}
-	if (y > n)
-	y = n;
-	s = malloc(sizeof(char) * (x + y + 1));
-	if (s == NULL)
-	return (NULL);
-	for (z = 0; z < x; z++)
-	s[z] = s1[z];
-	for (z = 0; z < y; z++)
-	s[z + x] = s2[z];
-	s[x + y] = '\0';
-	return (s);
-	}
+	return (mem);
+	}	
