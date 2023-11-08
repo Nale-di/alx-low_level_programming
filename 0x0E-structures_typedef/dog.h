b98f82957ff5d67548dabaa8e1b2e517ee34b570
Nale-di <naledidironabeng@gmail.com>
2023-11-07 17:26:20 -0800

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,37 +1,20 @@
-#ifndef _DOG_H_
+#ifndef _DOG_H
 #define _DOG_H_
 
-
 /**
-  * struct dog - dog class
-  *
-  * @name: Name of the dog
-  *
-  * @age: Age of the dog
-  *
-  * @owner: Owner of the dog
-  */
-
-
-	struct dog
+*struct dog -a struct that create a dog info
+* 
+* @name: the name of the dog
+* @age: age of dog
+* @owner:the owner of dog
+*
+* Description: first struct with alx
+*/
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-
-typedef struct dog dog_t;
-
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-
-void free_dog(dog_t *d);
-
-
+};       
+void init_dog(struct dog *d, char *name, float age, char *OWNER);
 #endif
